58f0e7cad4120d66254bad0681dd71afefe692b3
Hamza Zubair <gondal.humza@gmail.com>
2021-10-27 03:32:10 +0200

    update fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://challengetask.herokuapp.com/'
-
-    resource '*',
-      headers: :any,
-      methods: [:get]
-  end
+     origins '*'
+     resource '*', :headers => :any, :methods => [:get, :post, :options]
+   end
 end
